544364a8c649956b40e3da630dbb45ff6a0e1693
Roy Gardner <roybowesgardner@gmail.com>
2025-07-23 19:01:21 +0100

    Tidied up packages and utilities

--- analysis/_library/packages.py
+++ analysis/_library/packages.py
@@ -9,15 +9,6 @@ import csv
 from datetime import datetime, timedelta
 from decimal import *
 
-from IPython.core.display import HTML
-from ipywidgets import interact, interactive, fixed, interact_manual
-from ipywidgets import Layout, HBox, VBox
-import ipywidgets as widgets
-from IPython.display import Image, display, clear_output
-from IPython.display import Javascript
-
-import networkx as nx
-
 import json
 import math
 import matplotlib as mpl
@@ -25,22 +16,11 @@ import matplotlib.pyplot as plt
 from matplotlib_venn import venn2
 import numpy as np
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-import pandas as pd
 
 import random
 
 import scipy as sp
-from scipy import stats
-from scipy.cluster.hierarchy import linkage, dendrogram
-from scipy.spatial.distance import *
-from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import connected_components
-import seaborn as sns
 import string
 
-import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_text
 import time
 
